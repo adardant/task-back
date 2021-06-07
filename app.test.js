979980ac0a30e4f47c3ef1cb2e22a2adbe5f5b3f
adardant <aureliendardant@hotmail.fr>
2021-06-07 15:11:44 +0200

    modif

--- app.test.js
+++ app.test.js
@@ -16,24 +16,24 @@ describe('Check routes', () => {
         const res = await request(app)
             .get('/task/5fd220fb8541d83690cf8ee8')
         expect(res.statusCode).toEqual(200);
-    })
+    }, 30000)
 
     it('should create a new post', async () => {
         const res = await request(app)
             .get('/task')
         expect(res.statusCode).toEqual(200);
-    })
+    }, 30000)
 
     it('success add task', async () => {
         const res = await request(app)
             .post('/task')
         expect(res.statusCode).toEqual(200);
-    })
+    }, 30000)
 
     it('success delete task', async () => {
         const res = await request(app)
             .get('/deleteTask/5fd220fb8541d83690cf8ee8')
         expect(res.statusCode).toEqual(200);
-    })
+    }, 30000)
 
 })
